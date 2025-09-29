ff2e8fe8262245ac32596f60ae5745c5d02ddfba
James Honzatko <bejbej.uit@gmail.com>
2025-09-29 11:51:20 -0500

    Use CSS animations for the small loading icon.

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,37 +1,10 @@
-import { Component, ElementRef, NgZone, OnDestroy, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-spinner",
     templateUrl: "./spinner.component.html",
+    styleUrl: "./spinner.component.scss",
     standalone: false
 })
-export class SpinnerComponent implements OnInit, OnDestroy {
-
-    index = 0;
-    states = "0123".split("");
-    intervalId = undefined;
-
-    constructor(private myElement: ElementRef, private ngZone: NgZone) { }
-
-    ngOnInit() {
-        let element = this.myElement.nativeElement.childNodes[0];
-
-        let spin = () => {
-            element.textContent = this.states[this.index];
-            this.index = this.index + 1 >= this.states.length ? 0 : this.index + 1;
-        }
-
-        spin();
-        this.ngZone.runOutsideAngular(() => {
-            this.intervalId = window.setInterval(spin, 150);
-        });
-    }
-
-    ngOnDestroy() {
-        if (this.intervalId) {
-            this.ngZone.runOutsideAngular(() => {
-                window.clearInterval(this.intervalId);
-            });
-        }
-    }
+export class SpinnerComponent {
 }
